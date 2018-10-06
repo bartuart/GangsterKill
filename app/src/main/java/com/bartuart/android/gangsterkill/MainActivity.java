1a51f7c5f18f005cb4320dafaf922ba29d0197b6
bartuart <arturs.bartusevics@tieto.com>
2018-10-07 00:24:07 +0300

    Added images

--- app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
@@ -2,6 +2,8 @@ package com.bartuart.android.gangsterkill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView im = findViewById(R.id.gangster);
+
+        im.setX(20);
+        im.setY(40);
     }
 }
