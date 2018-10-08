4a49d20e1cdd4383f05cafce70ecb671f8529cfe
bartuart <arturs.bartusevics@tieto.com>
2018-10-08 16:01:01 +0300

    Created first version of game gui.

--- app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
@@ -2,6 +2,7 @@ package com.bartuart.android.gangsterkill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -12,9 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView im = findViewById(R.id.gangster);
+        /*ImageView im = findViewById(R.id.gangster);
 
-        im.setX(20);
-        im.setY(40);
+        im.setX(200);
+        im.setY(150);
+        im.setVisibility(View.GONE);
+
+        im.setX(250);
+        im.setY(150);
+        im.setVisibility(View.VISIBLE);*/
     }
 }
