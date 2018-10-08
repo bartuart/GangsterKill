233158713adcb81915d2d5a1687af00a832cf0d5
bartuart <arturs.bartusevics@tieto.com>
2018-10-08 21:32:26 +0300

    Refactored to use my own movement formula

--- app/src/main/java/com/bartuart/android/gangsterkill/CitizenAnimationView.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/CitizenAnimationView.java
@@ -15,28 +15,29 @@ public class CitizenAnimationView extends View {
     /**
      * Class representing the state of a star
      */
-    private static class Star {
+    private static class Citizen {
         private float x;
         private float y;
-        private float scale;
-        private float alpha;
+        private float b;
+        //private float scale;
+        //private float alpha;
         private float speed;
     }
 
     private static final int BASE_SPEED_DP_PER_S = 200;
-    private static final int COUNT = 32;
+    private static final int COUNT = 2;
     private static final int SEED = 1337;
 
     /** The minimum scale of a star */
-    private static final float SCALE_MIN_PART = 0.45f;
+    //private static final float SCALE_MIN_PART = 0.45f;
     /** How much of the scale that's based on randomness */
-    private static final float SCALE_RANDOM_PART = 0.55f;
+    //private static final float SCALE_RANDOM_PART = 0.55f;
     /** How much of the alpha that's based on the scale of the star */
-    private static final float ALPHA_SCALE_PART = 0.5f;
+    //private static final float ALPHA_SCALE_PART = 0.5f;
     /** How much of the alpha that's based on randomness */
-    private static final float ALPHA_RANDOM_PART = 0.5f;
+    //private static final float ALPHA_RANDOM_PART = 0.5f;
 
-    private final Star[] mStars = new Star[COUNT];
+    private final Citizen[] mStars = new Citizen[COUNT];
     private final Random mRnd = new Random(SEED);
 
     private TimeAnimator mTimeAnimator;
@@ -77,19 +78,19 @@ public class CitizenAnimationView extends View {
         // The starting position is dependent on the size of the view,
         // which is why the model is initialized here, when the view is measured.
         for (int i = 0; i < mStars.length; i++) {
-            final Star star = new Star();
-            initializeStar(star, width, height);
-            mStars[i] = star;
+            final Citizen citizen = new Citizen();
+            initializeStar(citizen, width, height);
+            mStars[i] = citizen;
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         final int viewHeight = getHeight();
-        for (final Star star : mStars) {
+        for (final Citizen citizen : mStars) {
             // Ignore the star if it's outside of the view bounds
-            final float starSize = star.scale * mBaseSize;
-            if (star.y + starSize < 0 || star.y - starSize > viewHeight) {
+            final float starSize = mBaseSize;
+            if (citizen.y + starSize < 0 || citizen.y - starSize > viewHeight) {
                 continue;
             }
 
@@ -97,16 +98,16 @@ public class CitizenAnimationView extends View {
             final int save = canvas.save();
 
             // Move the canvas to the center of the star
-            canvas.translate(star.x, star.y);
+            canvas.translate(citizen.x, citizen.y);
 
             // Rotate the canvas based on how far the star has moved
-            final float progress = (star.y + starSize) / viewHeight;
-            canvas.rotate(360 * progress);
+            //final float progress = (citizen.y + starSize) / viewHeight;
+            //canvas.rotate(360 * progress);
 
             // Prepare the size and alpha of the drawable
             final int size = Math.round(starSize);
             mDrawable.setBounds(-size, -size, size, size);
-            mDrawable.setAlpha(Math.round(255 * star.alpha));
+            //mDrawable.setAlpha(Math.round(255 * citizen.alpha));
 
             // Draw the star to the canvas
             mDrawable.draw(canvas);
@@ -179,45 +180,50 @@ public class CitizenAnimationView extends View {
         final int viewWidth = getWidth();
         final int viewHeight = getHeight();
 
-        for (final Star star : mStars) {
+        for (final Citizen citizen : mStars) {
             // Move the star based on the elapsed time and it's speed
-            star.y -= star.speed * deltaSeconds;
+            citizen.x += citizen.speed;
+            //citizen.y -= citizen.speed * deltaSeconds;
+            citizen.y = citizen.speed * citizen.x + citizen.b;
 
             // If the star is completely outside of the view bounds after
             // updating it's position, recycle it.
-            final float size = star.scale * mBaseSize;
-            if (star.y + size < 0) {
-                initializeStar(star, viewWidth, viewHeight);
+            final float size = mBaseSize;
+            if (citizen.y + size < 0) {
+                initializeStar(citizen, viewWidth, viewHeight);
             }
         }
     }
 
     /**
      * Initialize the given star by randomizing it's position, scale and alpha
-     * @param star the star to initialize
+     * @param citizen the star to initialize
      * @param viewWidth the view width
      * @param viewHeight the view height
      */
-    private void initializeStar(Star star, int viewWidth, int viewHeight) {
+    private void initializeStar(Citizen citizen, int viewWidth, int viewHeight) {
         // Set the scale based on a min value and a random multiplier
-        star.scale = SCALE_MIN_PART + SCALE_RANDOM_PART * mRnd.nextFloat();
+        //citizen.scale = SCALE_MIN_PART + SCALE_RANDOM_PART * mRnd.nextFloat();
 
         // Set X to a random value within the width of the view
-        star.x = viewWidth * mRnd.nextFloat();
+        citizen.x = viewWidth * mRnd.nextFloat();
+
+        citizen.speed = new Random(10).nextFloat();
+        citizen.b = new Random(30).nextFloat();
 
         // Set the Y position
         // Start at the bottom of the view
-        star.y = viewHeight;
+        citizen.y = citizen.speed * citizen.x + citizen.b;
         // The Y value is in the center of the star, add the size
         // to make sure it starts outside of the view bound
-        star.y += star.scale * mBaseSize;
+        //citizen.y += citizen.scale * mBaseSize;
         // Add a random offset to create a small delay before the
         // star appears again.
-        star.y += viewHeight * mRnd.nextFloat() / 4f;
+        //citizen.y += viewHeight * mRnd.nextFloat() / 4f;
 
         // The alpha is determined by the scale of the star and a random multiplier.
-        star.alpha = ALPHA_SCALE_PART * star.scale + ALPHA_RANDOM_PART * mRnd.nextFloat();
+        //citizen.alpha = ALPHA_SCALE_PART * citizen.scale + ALPHA_RANDOM_PART * mRnd.nextFloat();
         // The bigger and brighter a star is, the faster it moves
-        star.speed = mBaseSpeed * star.alpha * star.scale;
+        //citizen.speed = mBaseSpeed * citizen.alpha * citizen.scale;
     }
 }
