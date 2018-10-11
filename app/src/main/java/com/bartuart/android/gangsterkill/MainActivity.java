160649444950c1d6246cc6f96e7006f9ccdb6ae7
bartuart <arturs.bartusevics@tieto.com>
2018-10-11 21:24:55 +0300

    type can be set - gangster or citizen

--- app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
@@ -24,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RelativeLayout game_layout = findViewById(R.id.main_game_field);
-        CitizenImageView image_view = new CitizenImageView(this);
+        CitizenImageView image_view = new CitizenImageView(this, R.mipmap.citizen);
         game_layout.addView(image_view);
 
-        CitizenImageView image_view1 = new CitizenImageView(this);
+        CitizenImageView image_view1 = new CitizenImageView(this, R.mipmap.gangster);
         game_layout.addView(image_view1);
 
-        CitizenImageView image_view2 = new CitizenImageView(this);
+        CitizenImageView image_view2 = new CitizenImageView(this, R.mipmap.citizen);
         game_layout.addView(image_view2);
 
     }
