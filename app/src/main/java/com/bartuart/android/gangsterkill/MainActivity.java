265314a3b1013202345f25e420b0c8992b534960
bartuart <arturs.bartusevics@tieto.com>
2018-10-11 20:46:52 +0300

    make source code clear

--- app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
@@ -17,70 +17,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CitizenAnimationView mAnimationView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         RelativeLayout game_layout = findViewById(R.id.main_game_field);
         CitizenImageView image_view = new CitizenImageView(this);
         game_layout.addView(image_view);
 
-
-        //mAnimationView = (CitizenAnimationView) findViewById(R.id.main_game_field);
-
-
-
-
-
-        //imageView = findViewById(R.id.citizen_image_view);
-
-        //info:
-        //https://medium.com/@patrick_iv/continuous-animation-using-timeanimator-5b8a903603fb
-
-
-        /*ObjectAnimator animation = ObjectAnimator.ofFloat(imageView, "translationX", 300f);
-        animation.setDuration(2000);
-        animation.start();
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ImageView current_view = (ImageView)v;
-                current_view.setImageResource(R.mipmap.gangster);
-            }
-        });
-
-        /*im.setX(200);
-        im.setY(150);
-        im.setVisibility(View.GONE);
-
-        im.setX(250);
-        im.setY(150);
-        im.setVisibility(View.VISIBLE);*/
     }
-
-    /*@Override
-    protected void onResume() {
-        super.onResume();
-        mAnimationView.resume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mAnimationView.pause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mAnimationView.pause();
-        mAnimationView = null;
-    }*/
 
 }
