5c459e1267f82eafb6f1ef45cfd838edbc8bfb67
bartuart <arturs.bartusevics@tieto.com>
2018-10-12 15:35:47 +0300

    removed unused import statements

--- app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
+++ app/src/main/java/com/bartuart/android/gangsterkill/MainActivity.java
@@ -1,22 +1,11 @@
 package com.bartuart.android.gangsterkill;
 
-import android.animation.ObjectAnimator;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.view.ViewTreeObserver;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
